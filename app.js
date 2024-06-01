89bb3db51e9ef999871bd90575ecdd164fe10aa7
Laura <paukstyte.laura@outlook.com>
2024-06-01 17:44:02 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const router = express.Router();
+const authRouter = require('./routes/authRoutes')
+const expenceCategoryRoute = require('./routes/expenseCategoryRoutes')
+const expenseController = require('./routes/expenseRoutes')
+// const servicesRouter = require('./routes/servicesRoute')
+// const foremanRouter = require('./routes/foremanRoute')
+app.use(express.json())
+
+app.use(morgan('dev'));
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/category', expenceCategoryRoute);
+app.use('/api/v1/expenses', expenseController)
+// app.use('/api/v1/users', usersRouter);
+// app.use('/api/v1/foreman', foremanRouter);
+
+
+module.exports = app;
---
+++ routes/expenseRoutes.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+const expenseController = require('../controllers/expenceController')
+const {authToken} = require('../middleware/authToken')
+
+
+router.route('/')
+.post( 
+    authToken, expenseController.createExpense
+)
+
+.get(
+    authToken, expenseController.getExpenses 
+)
+
+router.route('/:id')
+.delete(
+    authToken, expenseController.deleteExpense
+)
+.patch(
+    authToken, expenseController.updateExpenses
+)
+
+
+module.exports = router
